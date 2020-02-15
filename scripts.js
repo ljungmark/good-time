0dfd59e988c2e058e80884dddbe7d877f8e16ff7
ljungmark <mattias@ljungmark.co>
2020-02-15 16:12:17 +0100

    Update clock every minute, subset Inter

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
-(function () {
-    document.body.appendChild(document.createElement('time'));
-    document.querySelector('time').innerHTML = 'Hello!';
+(function update() {
     const date = new Date();
     document.querySelector('time').innerHTML = `${("00" + date.getHours()).slice(-2)}:${("00" + date.getMinutes()).slice(-2)}`;
+    setTimeout(update, 60000 - (Date.now() % 60000));
 })();
